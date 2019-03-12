3502ee2f4857f310738a7bd54b71e69da9712278
mervebirsin <mervebr34@outlook.com>
2019-03-12 20:36:19 +0300

    dotenv installation

--- app.js
+++ app.js
@@ -1,41 +1,76 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const redisStore = require('./helpers/redisStore');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dotenv = require('dotenv');
+dotenv.config();
 
-var app = express();
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+const chat = require('./routes/chat');
+const messages = require('./routes/messages');
+
+const app = express();
+
+// helpers
+const db = require('./helpers/db')();
+
+// middlewares
+const isAuthenticated = require('./middleware/isAuthenticated');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// express-session
+app.use(session({
+    store: redisStore,
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 14 * 24 * 3600000  }
+}));
+
+// passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/chat', isAuthenticated, chat);
+app.use('/messages', isAuthenticated, messages);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
